62abca14fd4fe3b852646923613d0cf26308e7b6
Naveen Kumar <bolla.naveenkumar@gmail.com>
2017-12-26 17:01:13 +0530

    go fmt changes

--- rango/vector.go
+++ rango/vector.go
@@ -11,35 +11,35 @@ type Vector struct {
 }
 
 func V(x float64, y float64, z float64) Vector {
-	return Vector{x, y, z }
+	return Vector{x, y, z}
 }
 
 func VecVecMult(a Vector, b Vector) Vector {
-	return V(a.X * b.X, a.Y * b.Y, a.Z * b.Z)
+	return V(a.X*b.X, a.Y*b.Y, a.Z*b.Z)
 }
 
 func FloatVecMult(a float64, b Vector) Vector {
-	return V(a * b.X, a * b.Y, a * b.Z)
+	return V(a*b.X, a*b.Y, a*b.Z)
 }
 
-func Dot(a Vector, b Vector) float64  {
-	return a.X * b.X + a.Y * b.Y + a.Z * b.Z
+func Dot(a Vector, b Vector) float64 {
+	return a.X*b.X + a.Y*b.Y + a.Z*b.Z
 }
 
 func Cross(a Vector, b Vector) Vector {
-	var x float64 = a.Y * b.Z - b.Y * a.Z
-	var y float64 = a.Z * b.X - b.Z * a.X
-	var z float64 = a.X * b.Y - b.X * a.Y
+	var x float64 = a.Y*b.Z - b.Y*a.Z
+	var y float64 = a.Z*b.X - b.Z*a.X
+	var z float64 = a.X*b.Y - b.X*a.Y
 
 	return V(x, y, z)
 }
 
 func Add(a Vector, b Vector) Vector {
-	return V(a.X + b.X, a.Y + b.Y, a.Z + b.Z)
+	return V(a.X+b.X, a.Y+b.Y, a.Z+b.Z)
 }
 
 func Subtract(a Vector, b Vector) Vector {
-	return V(a.X - b.X, a.Y - b.Y, a.Z - b.Z)
+	return V(a.X-b.X, a.Y-b.Y, a.Z-b.Z)
 }
 
 func Negate(v Vector) Vector {
@@ -47,7 +47,7 @@ func Negate(v Vector) Vector {
 }
 
 func Length(v Vector) float64 {
-	return math.Sqrt(v.X * v.X + v.Y * v.Y + v.Z * v.Z)
+	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
 }
 
 func Normalize(v Vector) Vector {
@@ -57,6 +57,5 @@ func Normalize(v Vector) Vector {
 		return v
 	}
 
-	return V(v.X / length, v.Y / length, v.Z / length)
+	return V(v.X/length, v.Y/length, v.Z/length)
 }
-
