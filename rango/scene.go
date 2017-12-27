10b6403d3b315437eea9cccb227ce8872ababcda
Naveen Kumar <bolla.naveenkumar@gmail.com>
2017-12-27 16:34:44 +0530

    [ENH] Add first version of object.go and scene.go

---
+++ rango/scene.go
@@ -0,0 +1,7 @@
+package rango
+
+type Scene struct {
+	nObject uint64   /* Number of objects in the scene */
+	nMax    uint64   /* Max number of objects scene can hold */
+	obj     []Object /* Object list of the Scene */
+}
